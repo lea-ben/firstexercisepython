63126130c4061e0f29a7512de6ecaea928d4fc08
lea-ben <leabenillouche@MacBook-Pro-de-Lea.local>
2021-01-31 15:08:04 +0200

    first commit

---
+++ exercise1.py
@@ -0,0 +1,85 @@
+# name = "Fred"
+# age=22
+
+
+
+# # "Hello Fred, you are 22 years old "
+
+# # print(f"hello {name} you are {age} years old")
+
+# email = input ("enter an email adress : ")
+
+# if "@" not in email or ".com" not in email: 
+#     print ("invalid adress")
+# else: 
+#     print("added to spam list")
+
+
+#                   EXERCISE 1
+# a = ("hello world \n")*4
+
+# print(a)
+
+#exercice 2
+# a=(99^3) * 8
+# print(a)
+
+#                   exercice 3
+
+##>>> print(5<3) # false
+#>>> print (3 == 3) # true
+#>>> print (3 == "3") # false
+#>>> print ("3" > 3) 3 # error
+#>>> print("Hello" == "hello") # false
+
+#                       exercise 4 
+
+#computer_brand = ("razer")
+
+
+#print(f"i have a {computer_brand} computer ")
+
+#                           EXERCISE 5
+
+#name = "dalida"
+#age = 108
+#shoe_size= 48
+
+#info = (f"my name is {name} i have {age} years old, and my size is {shoe_size}")
+
+#print(info)
+
+#                           EXERCISE 6
+
+#a=2
+#b=3
+
+#if ( a < b ) :
+ #    print ("hello world")
+
+# a=int(input("give me a number"))
+
+# if a%2==0 :
+#     print ("this is an even number")
+# else :
+#     print ("this is an odd number")
+
+#exercise 8
+
+# my_name = ("lea")
+# user_name = input("what is your name")
+
+# if my_name == user_name:
+#     print("AHAH")
+
+# exercise 9
+
+size = int(input("what is your size little man"))
+
+
+if size >= 145 :
+    print("you can") 
+else :
+    print("you cant")
+
+
