562acd48ce76a49b1b78d97fbceb843644278498
Leontine <73586854+lea-ben@users.noreply.github.com>
2021-01-31 17:21:56 +0200

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,39 @@
+#                 Exercise 1
+
+# print (" hello world \n"*4)
+# print (" I love python \n" *4)
+
+# month = int(input("give me a month with number 1 to 12"))
+
+# if month >= 5 :
+#     print("spring runs")
+# elif month 6 >= 8 :
+#     print("summer runs")
+# elif month 9>11 : 
+#     print("autumn runs")
+# elif month 12>2 : 
+#     print("winter runs")
+
+
+# print (bool(5 == "5"))
+
+# print(bool(bool(None)))
+
+# my_text = "Lorem ipsum dolor sit amet "
+
+# x = len(my_text)
+
+# print(x)
+
+#exercise 4
+
+# phrase=input("longest sentence without A ")
+
+
+while True : 
+    phrase = input("longest sentence without A ")
+
+    if "A" not in phrase:
+
+        print("congrat")         
+
